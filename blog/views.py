b4b89800eae4f663c207717f97400e5825111375
newtakin <33094236+newtakin@users.noreply.github.com>
2018-02-28 07:10:05 +0700

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from	django.shortcuts	import	redirect
 
 # Create your views here.
 def  post_list(request):
-	posts = Post.objects.order_by('published_date')
+	posts = Post.objects.all()
 	return render(request, 'blog/post_list.html', {'posts':posts})
 def  post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
